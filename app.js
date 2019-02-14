e1992ace7bf8e18ccaf6f206fc2799c72d2de563
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2019-02-14 00:03:24 +0000

    error handling in place

--- app.js
+++ app.js
@@ -22,8 +22,15 @@ request({
     url: `http://www.mapquestapi.com/geocoding/v1/address?key=gUOl7687qTZWzReEfmuKDtmnC7v3ixtJ&location=${encodedAddress}`,
     json: true
 } , (error, response, body) => {
-    // console.log(JSON.stringify(body, undefined,2));
-    console.log(`Address: ${body.results[0].providedLocation.location}, ${body.results[0].locations[0].postalCode}, ${body.results[0].locations[0].adminArea3}`)
-    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+    if (error) {
+        console.log('Unable to connect to MapQuest Servers')
+    } else if (body.results[0].locations[0].geoQualityCode === 'A1XAX') {
+        console.log('Unable to find that address');
+    } else {
+        console.log(`Address: ${body.results[0].providedLocation.location}`);
+        console.log(`Street: ${body.results[0].locations[0].street}`);
+        console.log(`PostCode: ${body.results[0].locations[0].adminArea3}`);
+        console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+        console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+    }
 });
